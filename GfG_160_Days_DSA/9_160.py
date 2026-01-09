10e75d0735d9e0eab8cb5fe5b1829fca61e5aadb
Vishal Sai Vardhan <vishal10102006@gmail.com>
2026-01-09 18:06:42 +0530

    GfG 9 of 160 days

---
+++ GfG_160_Days_DSA/9_160.py
@@ -0,0 +1,16 @@
+def getMinDiff(arr, k):
+    n = len(arr)
+    arr.sort()
+    res = arr[n - 1] - arr[0]
+    for i in range(1, len(arr)):
+        if arr[i] - k < 0:
+            continue
+        minH = min(arr[0] + k, arr[i] - k)
+        maxH = max(arr[i - 1] + k, arr[n - 1] - k)
+        res = min(res, maxH - minH)
+        return res
+if __name__ == "__main__":
+    k = 2
+    arr = [1, 5, 8, 10]
+    ans = getMinDiff(arr, k)
+    print(ans)
